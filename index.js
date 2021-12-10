e04b7ffacf3cbf114ecd28eac218281c2834b82c
Talwind1 <talwind1@gmail.com>
2021-12-10 18:07:07 +0200

    covid array data

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 //   australia: australia,
 // };
 
-let countries = [];
+let countryCode = "AF";
+let region_name = "asia";
+let myChart = document.getElementById("myChart").getContext("2d");
 
 async function fetchCountries(region_name) {
   const res = await fetch(
@@ -17,6 +19,7 @@ async function fetchCountries(region_name) {
     throw Error("region data not found");
   }
   const data = await res.json();
+  let countries = [];
 
   for (let i = 0; i < data.length; i++) {
     let countryData = {
@@ -25,12 +28,11 @@ async function fetchCountries(region_name) {
     };
     countries.push(countryData);
   }
-
   return countries;
 }
 
-async function fetchCovidData(countryCode) {
-  const url = await fetch(`https://corona-api.com/countries/${countryCode}`);
+async function fetchCovidData(country) {
+  const url = await fetch(`https://corona-api.com/countries/${country.code}`);
   if (!url.ok) {
     throw Error("covid country data not found");
   }
@@ -38,22 +40,68 @@ async function fetchCovidData(countryCode) {
   const latest_data = data.data.latest_data;
 
   const countryData = {
+    //  name: countryCode.name,
     deaths: latest_data.deaths,
     confirmed: latest_data.confirmed,
     recovered: latest_data.recovered,
     critical: latest_data.critical,
   };
-  console.log(countryData);
+  // console.log(countryData);
   return countryData;
 }
 
-let countryCode = "AF";
-let region_name = "asia";
+// function arrayCovid(countries, type) {
+//   const promisesArray = [];
+//   countries.forEach((country) => {
+//     promisesArray.push(fetchCovidData(country));
+//   });
+//   return promisesArray;
+// }
 
-fetchCountries(region_name).then((data) => {
-  data.forEach((element) => {
-    fetchCovidData(element.code);
-  });
-});
+// async function createGraph(tableData) {
+//   const myGraph = new Chart(myChart, {
+//     type: "bar",
+//     data: {
+//       datasets: tableData.deaths,
+//       labels: "ddd",
+//     },
+//     options: {},
+//   });
+// }
+//   let names = [];
+//   let data = [];
+//   tableData.forEach((element) => {
+//     names.push(element.name);
+//   });
+//   tableData.forEach((element) => {
+//     data.push(element.deaths);
+//   });
+//   const myGraph = new Chart(myChart, {
+//     type: "bar",
+//     data: {
+//       datasets: data,
+//       labels: names,
+//     },
+//     options: {},
+//   });
+// }
 
-async function createGraph() {}
+// fetchCountries(region_name)
+//   .then((data) => {
+//     data.forEach((element) => {
+//       fetchCovidData(element.code);
+//     });
+//   })
+//   .then((tableData) => {
+//     createGraph(tableData);
+//   });
+fetchCountries(region_name)
+  .then((countries) => {
+    const arrCovid = [];
+    countries.forEach((country) => {
+      arrCovid.push(fetchCovidData(country));
+    });
+    return arrCovid;
+  })
+  .then((arrCovid) => Promise.all(arrCovid))
+  .then((data) => console.log(data));
