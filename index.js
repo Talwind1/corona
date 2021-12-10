9c9168628387844efeccfe84c9f6bafa42e2e004
Talwind1 <talwind1@gmail.com>
2021-12-10 14:27:56 +0200

    2 fetching

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 //   australia: australia,
 // };
 
+let countries = [];
+
 async function fetchCountries(region_name) {
   const res = await fetch(
     `https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${region_name}`
@@ -15,23 +17,35 @@ async function fetchCountries(region_name) {
     throw Error("region data not found");
   }
   const data = await res.json();
-  const countries = [];
+
   for (let i = 0; i < data.length; i++) {
     countries.push(data[i].cca2);
   }
-  console.log(countries);
+
+  return countries;
 }
 
 let region_name = "asia";
-fetchCountries(region_name);
+fetchCountries(region_name).then((data) => console.log(data));
 
-
-async function fetchCovidData(countryCode){
+async function fetchCovidData(countryCode) {
   const url = await fetch(`https://corona-api.com/countries/${countryCode}`);
-  console.log(data);
-  if (url.ok) {
-    const data = await url.json();
-    
+  if (!url.ok) {
+    throw Error("covid country data not found");
+  }
+  const data = await url.json();
+  const latest_data = data.data.latest_data;
+
+  const countryData = {
+    deaths: latest_data.deaths,
+    confirmed: latest_data.confirmed,
+    recovered: latest_data.recovered,
+    critical: latest_data.critical,
+  };
+  console.log(countryData);
+  return countryData;
 }
-let countryCode= 'AF';
+
+let countryCode = "AF";
 fetchCovidData(countryCode);
+function collectData(dataType, countries) {}
